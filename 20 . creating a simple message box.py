0e118a2b5c66cf1ebdb5cb39b561d9f7b69a06da
Mayank Singh <51783955+mayank-siingh@users.noreply.github.com>
2019-06-30 21:10:45 +0530

    Add files via upload

---
+++ 20 . creating a simple message box.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox as mBox
+from tkinter import Tk
+root = Tk()
+root.withdraw()
+mBox.showinfo('Python Message Info Box','This is Python 3.7 :\nYour are asking for invalid request')
